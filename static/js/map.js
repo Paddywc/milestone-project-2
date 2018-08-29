df912ca4374423b5b33c3e4826f44c26ef7fb79e
Paddy Cremen <cremenp@tcd.ie>
2018-08-29 11:55:16 +0000

    Add searchbox functionality

--- static/js/map.js
+++ static/js/map.js
@@ -6,6 +6,61 @@ let destinationExplorerData = {
     numberOfCallsRunning: 0
 };
 
+
+// Adds functionality to Google Maps Searcj
+// Code from Google API documentation: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox.
+function createSearchbox(map) {
+  let input = document.getElementById('pac-input');
+  let searchBox = new google.maps.places.SearchBox(input)
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input)
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    let places = searchBox.getPlaces();
+
+    // Clear out the old markers.
+    markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    let bounds = new google.maps.LatLngBounds()
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log('Returned place contains no geometry');
+        return;
+      }
+      let icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+
 function toggleButtonActiveClass(button) {
     if ($(button).hasClass("disabled")) {
         $(button).removeClass("disabled");
@@ -464,10 +519,12 @@ $("#search-btn").click(function() {
 
 function initMapDestinationExplorer() {
     destinationExplorerData.map = generateNewMap();
+    createSearchbox(destinationExplorerData.map);
     destinationExplorerData.map.addListener('idle', function() {
-        //   addDataAndUpdateMap()  
         if ($(".filter-btn").hasClass("active")) {
             showSearchButton();
         }
     });
 };
+
+
